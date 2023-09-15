21db4c70d405e5fb05d23fab2850198d068c273a
Thierry Murhondezi Bisimwa <thierrymurhondezi@gmail.com>
2023-09-15 01:07:47 +0100

    add : an explanation about objects

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // Objects in JavaScript are a little more complex than arrays, but they're also very powerful. An object is a data structure that can contain multiple properties and methods. Each property has a name and a value, and each method is a function that can be executed
+
+
+Here's an example of a simple object:
+- var myObject = { name: "John", age: 23, hobby: "hiking" };
+This object has three properties: name, age, and hobby. Each property has a different value. You can access the properties of an object using dot notation, like this:
+- myObject.name; //
